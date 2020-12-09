408dce17d3eda157266865d168d484b606d3b15f
Christopher Jaeger <cjaeger.ld@gmail.com>
2020-12-09 13:24:44 -0500

    Added letter checking and displaying

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,71 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor() {
+        missed = 0;
+        phrases = [ 'breath of fire',
+                    'zelda',
+                    'mario party',
+                    'final fantasy',
+                    'illusion of gaia' ];
+        activePhrase = null;
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    startGame() {
+
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    getRandomPhrase() {
+
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    handleInteraction() {
+
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    removeLife() {
+
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    checkForWin() {
+
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    gameOver() {
+        
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,56 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+        this.phraseDisplay = document.querySelector('#phrase ul');
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    addPhraseToDisplay() { 
+        const characters = this.phrase.split('');
+        characters.forEach(character => {
+            const display = document.createElement('li');
+            if (character !== ' ') {
+                display.setAttribute('class', `hide letter ${character}`);
+                display.textContent = character;
+            } else {
+                display.setAttribute('class', 'space');
+            }
+            this.phraseDisplay.appendChild(display);
+        });
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    checkLetter(character) {
+        const letterObjects = this.phraseDisplay.querySelectorAll('li');
+        letterObjects.forEach(letterObject => {
+            const letter = letterObject.getAttribute('class').split('').pop();
+            if (letter === character) {
+                this.showMatchedLetter(letterObject);
+            }
+        });
+    }
+
+    /**
+     * Adds letter placeholders to the display when game starts
+     * @param {number} x The number to raise.
+     * @param {number} n The power, must be a natural number.
+     * @return {number} x raised to the n-th power.
+     */
+    showMatchedLetter(letter) {
+        letter.classList.remove('hide');
+        letter.classList.add('show');
+    }
+}
+const display = new Phrase('test this phrase');
+display.addPhraseToDisplay();
+display.checkLetter('t');
